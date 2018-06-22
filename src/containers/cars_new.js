3bac62973e97764d51157d854096a48f53bc02b4
Reka Kulinyi <reekaa85@gmail.com>
2018-06-22 16:55:19 +0200

    form changed

--- src/containers/cars_new.js
+++ src/containers/cars_new.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { connect } from "react-redux";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import { addCar } from "../actions";
 
 class CarsNew extends Component {
@@ -18,6 +19,7 @@ class CarsNew extends Component {
         <input
           className="form-control"
           type={field.type}
+          placeholder={field.placeholder}
           {...field.input}
         />
       </div>
@@ -28,34 +30,36 @@ class CarsNew extends Component {
   render(){
     return(
       <div>
+        <h2>Add a car to {this.props.garage} garage</h2>
+        <Link to="/">Back to cars</Link>
         <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <Field
-            label="Brand"
             name="brand"
             type="text"
+            label="Brand"
+            placeholder="Ford"
             component={this.renderField}
-
           />
           <Field
             label="Model"
             name="model"
             type="text"
             component={this.renderField}
-
+            placeholder="Mustang"
           />
           <Field
             label="Owner"
             name="owner"
             type="text"
             component={this.renderField}
-
+            placeholder="John Smith"
           />
           <Field
             label="Plate"
             name="plate"
             type="text"
             component={this.renderField}
-
+            placeholder="123ABC"
           />
 
           <button className="btn btn-primary" type="submit"
@@ -68,5 +72,11 @@ class CarsNew extends Component {
   }
 }
 
+function mapStateToProps(reduxState){
+  return{
+    garage: reduxState.garage
+  };
+}
 
-export default reduxForm({ form: 'newCarForm' })(connect(null, {addCar})(CarsNew));
+
+export default reduxForm({ form: 'newCarForm' })(connect(mapStateToProps, {addCar})(CarsNew));
